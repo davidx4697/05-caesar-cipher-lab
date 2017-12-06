f4364bfa807156cf1b6fed2e45c2c1977206b502
davidx4697 <davidx4697@hstat.org>
2017-12-06 19:27:19 +0000

    revised, done

--- lib/caesar.rb
+++ lib/caesar.rb
@@ -17,20 +17,5 @@ cphrase = phrase.split("")
    return cc_done = cphrase.join
 end
 def caesar_decode(phrase, offset)
-    al= [*('a'..'z')]
-cphrase = phrase.split("")
-  cphrase.map.with_index do |letter, index|
-    al.each_with_index do |letter2, index2|
-      if letter =~ (/[A-Z]/)
-        if letter.downcase == letter2
-          cphrase[index] = al[(index2-offset)%26].upcase
-        end
-        else
-          if letter == letter2
-          cphrase[index] = al[(index2-offset)%26]
-          end
-      end
-    end
-  end   
-   return cc_done = cphrase.join
+    caesar_encode(phrase, -offset)
 end
